c3253e22c59a78d56ab00cb55eb5b4d5fa85e163
htchuc26 <thanhchuc.cnttk38@gmail.com>
2021-05-06 13:26:14 +0700

    Chapter_7: Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     root "static_pages#home"
     get "static_pages/home"
     get "static_pages/help"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new show create)
   end
 end
-
